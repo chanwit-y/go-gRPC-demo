5543de4430385066ceea64d361d1343fafa0ec84
chanwit-y <chanwit.yimneam@hotmail.com>
2023-06-19 22:16:50 +0700

    gen proto buff

---
+++ proto/gen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+protoc demo.proto --go_out=../server --go-grpc_out=../server
+# protoc --proto_path=server --go_out=plugins=grpc:. proto/demo.proto
+# protoc subscription.proto --go_out=../server --go-grpc_out=../server
