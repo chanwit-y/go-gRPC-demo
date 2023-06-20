9403f723b6e238ab46ab4852e17762c3b11a470f
chanwit-y <chanwit.yimneam@hotmail.com>
2023-06-20 07:22:38 +0700

    gen demo_grpc

--- gen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# protoc demo.proto --go_out=../server --go-grpc_out=../server
-# protoc --proto_path=server --go_out=plugins=grpc:. proto/demo.proto
-# protoc subscription.proto --go_out=../server --go-grpc_out=../server
