dc75aabc628f6c114165e0deb8739c647d7c4261
chanwit-y <chanwit.yimneam@hotmail.com>
2023-06-21 21:10:53 +0700

    stream data

--- client/main.go
+++ client/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"grpc-client/service"
+	"io"
 	"log"
-	"time"
 
 	"google.golang.org/grpc"
 )
@@ -12,29 +12,56 @@ import (
 func main() {
 	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
+		log.Fatalf("Failed to connect: %v", err)
 	}
 	defer conn.Close()
 
 	client := service.NewLongTimeRequestServiceClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	stream, err := client.LongTimeRequestStream(ctx)
+	// req := &service.Request{Data: "Test data"}
+	stream, err := client.LongTimeRequestStream(context.Background())
 	if err != nil {
-		log.Fatalf("Error on get stream: %v", err)
+		log.Fatalf("Error while calling LongTimeRequestStream: %v", err)
 	}
 
-	req := service.Request{Data: "Test data"}
-	if err := stream.Send(&req); err != nil {
-		log.Fatalf("Error while sending stream: %v", err)
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break // The server closed the stream.
+		}
+		if err != nil {
+			log.Fatalf("Error while receiving stream: %v", err)
+		}
+		log.Printf("Response from server: %s", res.GetResult())
 	}
-
-	res, err := stream.Recv()
-	if err != nil {
-		log.Fatalf("Error while receiving stream: %v", err)
-	}
-
-	log.Printf("Response from server: %s", res.GetResult())
 }
+
+// func main() {
+// 	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
+// 	if err != nil {
+// 		log.Fatalf("Did not connect: %v", err)
+// 	}
+// 	defer conn.Close()
+
+// 	client := service.NewLongTimeRequestServiceClient(conn)
+
+// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+// 	defer cancel()
+
+// 	stream, err := client.LongTimeRequestStream(ctx)
+// 	if err != nil {
+// 		log.Fatalf("Error on get stream: %v", err)
+// 	}
+
+// 	req := service.Request{Data: "Test data"}
+// 	if err := stream.Send(&req); err != nil {
+// 		log.Fatalf("Error while sending stream: %v", err)
+// 	}
+
+// 	res, err := stream.Recv()
+// 	if err != nil {
+// 		log.Fatalf("Error while receiving stream: %v", err)
+// 	}
+
+// 	log.Printf("Response from server: %s", res.GetResult())
+// }
--- server/service/demo_service.go
+++ server/service/demo_service.go
@@ -3,6 +3,8 @@ package service
 import (
 	"io"
 	"log"
+	"strconv"
+	"time"
 )
 
 type demoService struct{}
@@ -14,8 +16,25 @@ func NewDemoService() demoService {
 func (d demoService) mustEmbedUnimplementedLongTimeRequestServiceServer() {}
 
 func (d demoService) LongTimeRequestStream(stream LongTimeRequestService_LongTimeRequestStreamServer) error {
+	// for {
+	// 	req, err := stream.Recv()
+	// 	if err == io.EOF {
+	// 		// The client has closed the stream.
+	// 		return nil
+	// 	}
+	// 	if err != nil {
+	// 		log.Fatalf("Error while reading client stream: %v", err)
+	// 	}
+
+	// 	// Handle the request and send a response.
+	// 	response := Response{Result: "Received: " + req.GetData()}
+	// 	if err := stream.Send(&response); err != nil {
+	// 		log.Fatalf("Error while sending response to client: %v", err)
+	// 	}
+	// }
+
 	for {
-		req, err := stream.Recv()
+		_, err := stream.Recv()
 		if err == io.EOF {
 			// The client has closed the stream.
 			return nil
@@ -25,9 +44,14 @@ func (d demoService) LongTimeRequestStream(stream LongTimeRequestService_LongTim
 		}
 
 		// Handle the request and send a response.
-		response := Response{Result: "Received: " + req.GetData()}
-		if err := stream.Send(&response); err != nil {
-			log.Fatalf("Error while sending response to client: %v", err)
+		for i := 0; i < 10; i++ {
+			res := &Response{Result: "Message " + strconv.Itoa(i)}
+			if err := stream.Send(res); err != nil {
+				return err
+			}
+			time.Sleep(1 * time.Second)
 		}
+		return nil
 	}
+
 }
