8e36d6cda0d23e2d691c2cf9422b16699f98c116
chanwit-y <chanwit.yimneam@hotmail.com>
2023-06-21 20:47:47 +0700

    client

--- proto/gen.sh
+++ proto/gen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 protoc demo.proto --go_out=../server --go-grpc_out=../server
+protoc demo.proto --go_out=../client --go-grpc_out=../client
 # protoc --proto_path=server --go_out=plugins=grpc:. proto/demo.proto
 # protoc subscription.proto --go_out=../server --go-grpc_out=../server
