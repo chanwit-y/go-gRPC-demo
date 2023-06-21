2d999895b67f3498527ce58b9f73e2bac27316bb
chanwit-y <chanwit.yimneam@hotmail.com>
2023-06-21 20:58:05 +0700

    add workspace

--- client/main.go
+++ client/main.go
@@ -1,4 +1,40 @@
 package main
 
+import (
+	"context"
+	"grpc-client/service"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
+	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := service.NewLongTimeRequestServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	stream, err := client.LongTimeRequestStream(ctx)
+	if err != nil {
+		log.Fatalf("Error on get stream: %v", err)
+	}
+
+	req := service.Request{Data: "Test data"}
+	if err := stream.Send(&req); err != nil {
+		log.Fatalf("Error while sending stream: %v", err)
+	}
+
+	res, err := stream.Recv()
+	if err != nil {
+		log.Fatalf("Error while receiving stream: %v", err)
+	}
+
+	log.Printf("Response from server: %s", res.GetResult())
 }
