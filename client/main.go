0c7a553f2ad5699a4f4de7f7147482228f8f9f60
chanwit-y <chanwit.yimneam@hotmail.com>
2023-06-25 21:56:01 +0700

    refactor to function

--- client/main.go
+++ client/main.go
@@ -4,70 +4,71 @@ import (
 	"context"
 	"fmt"
 	"grpc-client/service"
+	"io"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
 )
 
-// func main() {
-// 	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
-// 	if err != nil {
-// 		log.Fatalf("Failed to connect: %v", err)
-// 	}
-// 	defer conn.Close()
+func ReceiveStreamResponse() {
+	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
 
-// 	client := service.NewLongTimeRequestServiceClient(conn)
+	client := service.NewLongTimeRequestServiceClient(conn)
 
-// 	// req := &service.Request{Data: "Test data"}
-// 	stream, err := client.LongTimeRequestStream(context.Background())
-// 	if err != nil {
-// 		log.Fatalf("Error while calling LongTimeRequestStream: %v", err)
-// 	}
+	// req := &service.Request{Data: "Test data"}
+	stream, err := client.LongTimeRequestStream(context.Background())
+	if err != nil {
+		log.Fatalf("Error while calling LongTimeRequestStream: %v", err)
+	}
 
-// 	for {
-// 		res, err := stream.Recv()
-// 		if err == io.EOF {
-// 			break // The server closed the stream.
-// 		}
-// 		if err != nil {
-// 			log.Fatalf("Error while receiving stream: %v", err)
-// 		}
-// 		log.Printf("Response from server: %s", res.GetResult())
-// 	}
-// }
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break // The server closed the stream.
+		}
+		if err != nil {
+			log.Fatalf("Error while receiving stream: %v", err)
+		}
+		log.Printf("Response from server: %s", res.GetResult())
+	}
+}
 
-// func main() {
-// 	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
-// 	if err != nil {
-// 		log.Fatalf("Did not connect: %v", err)
-// 	}
-// 	defer conn.Close()
+func SendRequestStreama() {
+	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
 
-// 	client := service.NewLongTimeRequestServiceClient(conn)
+	client := service.NewLongTimeRequestServiceClient(conn)
 
-// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-// 	defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 
-// 	stream, err := client.LongTimeRequestStream2(ctx)
-// 	if err != nil {
-// 		log.Fatalf("Error on get stream: %v", err)
-// 	}
+	stream, err := client.LongTimeRequestStream2(ctx)
+	if err != nil {
+		log.Fatalf("Error on get stream: %v", err)
+	}
 
-// 	req := service.Request{Data: "Test data 1"}
-// 	if err := stream.Send(&req); err != nil {
-// 		log.Fatalf("Error while sending stream: %v", err)
-// 	}
+	req := service.Request{Data: "Test data 1"}
+	if err := stream.Send(&req); err != nil {
+		log.Fatalf("Error while sending stream: %v", err)
+	}
 
-// 	res, err := stream.Recv()
-// 	if err != nil {
-// 		log.Fatalf("Error while receiving stream: %v", err)
-// 	}
+	res, err := stream.Recv()
+	if err != nil {
+		log.Fatalf("Error while receiving stream: %v", err)
+	}
 
-// 	log.Printf("Response from server: %s", res.GetResult())
-// }
+	log.Printf("Response from server: %s", res.GetResult())
+}
 
-func main() {
+func GetResponseList() {
 	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Did not connect: %v", err)
@@ -85,4 +86,8 @@ func main() {
 	}
 
 	fmt.Printf("Response from server: %v", res.GetRequests())
+}
+
+func main() {
+	GetResponseList()
 }
